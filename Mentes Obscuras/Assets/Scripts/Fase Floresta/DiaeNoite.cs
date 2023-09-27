3625c0ce9968e2e7de0ec64621473cd94cb98f6c
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2023-09-27 17:55:01 -0300

    Att Character Config

---
+++ Mentes Obscuras/Assets/Scripts/Fase Floresta/DiaeNoite.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiaeNoite : MonoBehaviour
+{
+    [SerializeField] private float timeSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.rotation *= Quaternion.Euler(0, 0, timeSpeed * Time.deltaTime);
+    }
+}
