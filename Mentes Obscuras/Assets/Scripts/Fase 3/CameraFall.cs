7194de4c019746f2d6684223627eb089028ae8aa
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2023-09-27 16:11:07 -0300

    Att

---
+++ Mentes Obscuras/Assets/Scripts/Fase 3/CameraFall.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFall : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject targetFimdeFase;
+
+
+    void InicioFimdeFase()
+    {
+
+    }
+}
